1f44c6d02e81b5217f8fb8b90024fd8c07994568
TheDogHusky <classy.craft3r@gmail.com>
2022-06-17 19:54:00 +0200

    Optimized code, make it smaller and modified things to be better

--- test.js
+++ test.js
@@ -1,1 +1,122 @@
 console.log("pass")
+const helper = require("./index");
+
+let num1 = 1;
+let num2 = 5;
+let webhookurl = "";
+let embed = {
+  color: 0x0099ff,
+  title: "Some title",
+  url: "https://discord.js.org",
+  author: {
+    name: "Some name",
+    icon_url: "https://i.imgur.com/AfFp7pu.png",
+    url: "https://discord.js.org",
+  },
+  description: "Some description here",
+  thumbnail: {
+    url: "https://i.imgur.com/AfFp7pu.png",
+  },
+  fields: [
+    {
+      name: "Regular field title",
+      value: "Some value here",
+    },
+    {
+      name: "\u200b",
+      value: "\u200b",
+      inline: false,
+    },
+    {
+      name: "Inline field title",
+      value: "Some value here",
+      inline: true,
+    },
+    {
+      name: "Inline field title",
+      value: "Some value here",
+      inline: true,
+    },
+    {
+      name: "Inline field title",
+      value: "Some value here",
+      inline: true,
+    },
+  ],
+  image: {
+    url: "https://i.imgur.com/AfFp7pu.png",
+  },
+  timestamp: new Date(),
+  footer: {
+    text: "Some footer text here",
+    icon_url: "https://i.imgur.com/AfFp7pu.png",
+  },
+};
+
+let ip = "";
+let port = "";
+
+let helperclient = new helper.Client("Bots Name", Client);
+let helperutils = new helper.Utils("Name for logs");
+
+// console examples
+helperclient.executeconsole('info', "online"); // info
+helperclient.executeconsole('error', "online"); // error
+helperclient.executeconsole('warn', "online"); // warn
+// end of console examples
+
+helperclient.ping().then((data) => {
+  let { embed, text } = data;
+
+  //code here
+});
+
+let arrary = ["1", "2", "3", "4"];
+
+helperutils.pages(arrary, 2, 2).then((data) => {
+  console.log(data.array);
+  console.log(data.page);
+});
+
+//helperclient.channelsend(message.channel, "test", embed);
+//helperclient.channelsend(message.channel, "", embed);
+//helperclient.channelsend(message.channel, "test");
+
+helperutils.discordsendwebhook(webhookurl, embed);
+
+helperutils.versioninfo().then((data) => {
+  let { discordjs, node } = data;
+  //code here
+});
+
+helperutils.checkipport(ip, port).then((data) => {
+  console.log(data); // returns true or false
+});
+
+helperutils.math(1, 5).then((data) => {
+  let {
+    add,
+    addround,
+    subtract,
+    subtractround,
+    multiplication,
+    multiround,
+    division,
+    divisionround,
+    remainder,
+    exponent,
+    exponentround,
+  } = data;
+
+  console.log(add); //not rounded
+  console.log(addround); // rounded
+  console.log(subtract); // not rounded
+  console.log(subtractround); // rounded
+  console.log(multiplication); // not rounded
+  console.log(multiround); // rounded
+  console.log(division); // not rounded
+  console.log(divisionround); // rounded
+  console.log(remainder);
+  console.log(exponent); //not rounded
+  console.log(exponentround); // rounded
+});
